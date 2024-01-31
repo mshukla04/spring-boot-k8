156a430bce9f00919ccd60e456bfd82b51e7e646
maharshishukla4 <maharshi.shukla@zemosolabs.com>
2024-01-31 15:50:24 +0530

    spring service added

---
+++ service-one/src/main/java/com/example/serviceone/controller/ServiceOneController.java
@@ -0,0 +1,28 @@
+package com.example.serviceone.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ServiceOneController {
+
+    private final RestTemplate restTemplate;
+
+    public ServiceOneController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping("/send1")
+    public String getFromOtherService() {
+        String endPoint = "http://localhost:9090/api/v2/receive2";
+        return restTemplate.getForObject(endPoint, String.class);
+    }
+
+    @GetMapping("/receive1")
+    public String response(){
+        return "Sending response from service 1";
+    }
+}
---
+++ service-two/src/main/java/com/example/servicetwo/controller/ServiceTwoController.java
@@ -0,0 +1,28 @@
+package com.example.servicetwo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/api/v2")
+public class ServiceTwoController {
+
+    private final RestTemplate restTemplate;
+
+    public ServiceTwoController(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @GetMapping("/send2")
+    public String getFromOtherService() {
+        String endPoint = "http://localhost:8080/api/v1/receive1";
+        return restTemplate.getForObject(endPoint, String.class);
+    }
+
+    @GetMapping("/receive2")
+    public String response(){
+        return "Sending response from service 2";
+    }
+}
