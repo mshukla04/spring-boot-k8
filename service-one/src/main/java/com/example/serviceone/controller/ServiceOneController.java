4d665b40a4faf7a558b01abf181570c032b0dac0
maharshishukla4 <maharshi.shukla@zemosolabs.com>
2024-02-09 17:40:43 +0530

    change api call

--- service-one/src/main/java/com/example/serviceone/controller/ServiceOneController.java
+++ service-one/src/main/java/com/example/serviceone/controller/ServiceOneController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
-import java.net.URISyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -19,11 +21,38 @@ public class ServiceOneController {
     }
 
     @GetMapping("/send1")
-    public String getFromOtherService() throws URISyntaxException {
+    public String getFromOtherService() throws IOException {
 //        URI uri = new URI("http://service2-service:9090/api/v2/receive2");
         String endPoint = "https://jsonplaceholder.typicode.com/posts";
-        String response = restTemplate.getForObject(endPoint, String.class);
-        return response;
+        URL url = new URL(endPoint);
+
+        // Open connection
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        // Set request method (GET, POST, etc.)
+        connection.setRequestMethod("GET");
+
+        // Get response code
+        int responseCode = connection.getResponseCode();
+        System.out.println("Response Code: " + responseCode);
+
+        // Read response
+        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String line;
+        StringBuffer response = new StringBuffer();
+
+        while ((line = reader.readLine()) != null) {
+            response.append(line);
+        }
+        reader.close();
+
+        // Print response
+        System.out.println("Response: " + response.toString());
+
+        // Close connection
+        connection.disconnect();
+
+        return response.toString();
     }
 
     @GetMapping("/receive1")
